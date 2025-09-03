ecde5b154a32e01fc7aeedb1ff65938c108d87c9
Renata Santos <renatarhc@outlook.com>
2025-09-03 19:51:41 -0300

    Projeto iniciado

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { LoginScreen } from './user-module/login-screen/login-screen';
+
+export const routes: Routes = [
+
+    {
+
+        path: "login",
+        loadComponent: () => LoginScreen
+
+    }
+
+];
