44506c4886baf75378ef015bf056ec02d9d37ebd
Renata Santos <renatarhc@outlook.com>
2025-09-17 19:53:55 -0300

    Interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,34 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, inject, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter, Router } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const router = inject(Router);
+
+  return next(req).pipe(
+    catchError((err:HttpErrorResponse) => {
+
+      if (err.status == 401) {
+        //Token Expirou
+        localStorage.clear(); //Limpa todos os dados do localstorage.
+        router.navigate(['/login']); //Redireciona para o login.
+      }
+
+      return throwError(() => err);
+
+    })
+  );
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([ authInterceptor ])
+    ),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes)
